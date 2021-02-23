62f84c2ada923d29729c0b9ce07905620942b637
w0rng <antonabramov2000@gmail.com>
2021-02-23 13:55:21 +0700

    генерация тестовых значений

--- src/app.py
+++ src/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify
 from parsers import get_all_menu
 from os import popen
+from json import loads as json_loads
+from random import randint, choices
 
 
 app = Flask(__name__)
@@ -16,6 +18,21 @@ def get_menu():
     ).read()
 
     return jsonify(get_all_menu(menu)), 200
+
+
+@app.route('/menu/test/', methods=['GET'])
+def get_test_menu():
+    result = []
+    dataset = json_loads(open('test_dataset.json').read())
+
+    for room in dataset['rooms']:
+        for type in dataset['types']:
+            for dish in choices(dataset['types'][type], k=randint(2, 4)):
+                dish['type'] = type
+                dish['room'] = room
+                result.append(dish)
+
+    return jsonify(result)
 
 
 if __name__ == '__main__':
